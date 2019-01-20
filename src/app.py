8bfa6a89623cc338c9e327205bf21713f9634244
Jonathan Xu <jonathanj.xu97@gmail.com>
2019-01-19 19:58:07 -0600

    Add CORS, updated requirments

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 
 import src.auth as auth
 import src.admin as admin
@@ -8,21 +9,27 @@ from src.mongo import DBClient
 from models.user import User
 
 app = Flask(__name__)
+cors = CORS(app)
 client = DBClient()
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/', methods=['GET'])
+@cross_origin()
 def index():
     return 'EOH Volunteer API'
 
 @app.route('/register/', methods=['POST'])
+@cross_origin()
 def register():
     return auth.register(client, request)
 
 @app.route('/login/', methods=['POST'])
+@cross_origin()
 def login():
     return auth.login(client, request)
 
 @app.route('/delete_user/', methods=['POST'])
+@cross_origin()
 def remove():
     return auth.delete(client, request)
 
@@ -47,17 +54,21 @@ def admin_del_pos():
     return admin.admin_del_pos(client, request)
 
 @app.route('/get_all_timeslots/', methods=['GET'])
+@cross_origin()
 def get_all():
     return scheduler.get_all(client, request)
 
 @app.route('/add_timeslot/', methods=['POST'])
+@cross_origin()
 def add_timeslot():
     return scheduler.add_timeslot(client, request)
     
 @app.route('/get_timeslots/', methods=['GET'])
+@cross_origin()
 def get_timeslots():
     return scheduler.get_registered_timeslots(client, request)
 
 @app.route('/del_timeslot/', methods=['POST'])
+@cross_origin()
 def del_timeslot():
     return scheduler.remove_timeslot(client, request)
